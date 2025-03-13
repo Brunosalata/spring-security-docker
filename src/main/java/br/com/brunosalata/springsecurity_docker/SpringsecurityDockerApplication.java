680bce00414b52e1bac9f83bf5e10cf20728f505
Bruno Salata <brunoslima@gmail.com>
2025-03-13 07:42:11 -0300

    Project start

---
+++ src/main/java/br/com/brunosalata/springsecurity_docker/SpringsecurityDockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunosalata.springsecurity_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunosalata/springsecurity_docker/SpringsecurityDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunosalata.springsecurity_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
